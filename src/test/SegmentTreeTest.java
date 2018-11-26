29b322116217581017a1e99937c679c08b0fa28c
Administrator <admin@example.com>
2018-11-26 21:51:49 +0800

    '线段树查询指定区间功能实现'

--- src/test/SegmentTreeTest.java
+++ src/test/SegmentTreeTest.java
@@ -1,4 +1,18 @@
 package test;
 
+import tree.SegmentTree;
+
 public class SegmentTreeTest {
+
+    public static void main(String[] args) {
+        Integer[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //       18
+//           1-4  5-8
+//        1-2 3-4 5-6 7-8
+//       1  2  3  4  5  6  7  8
+
+        SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, (e, f) -> e + f);
+
+        System.out.println(segmentTree.query(0, 3));
+    }
 }
--- src/tree/SegmentTree.java
+++ src/tree/SegmentTree.java
@@ -1,5 +1,6 @@
 package tree;
 
+import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
 
 /**
@@ -40,13 +41,55 @@ public class SegmentTree<E> {
         int mid = l + (r - l) / 2;
 
         //构建左子树
-        bulidSegment(leftIndex, leftIndex, mid);
+        bulidSegment(leftIndex, l, mid);
         //创建右子树
         bulidSegment(rightIndex, mid + 1, r);
 
-        tree[treeIndex] =merger.merger(tree[leftIndex],tree[rightIndex]);
+        tree[treeIndex] = merger.merger(tree[leftIndex], tree[rightIndex]);
 
 
+    }
+
+    /**
+     * 查询区间值
+     *
+     * @param queryL 左边界
+     * @param queryR 右边界
+     * @return
+     */
+    public E query(int queryL, int queryR) {
+        //合法性判断
+        if (queryL > queryR || queryL < 0 || queryR > data.length)
+            throw new IllegalArgumentException("Segment is exist");
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    /**
+     * 从指定位置开始经行查找，递归
+     *
+     * @param treeIndex 线段树下标
+     * @param l         被查找区间开始点
+     * @param r         被查找区间结束点
+     * @param queryL    查找区间开始点
+     * @param queryR    查找区间结束点
+     * @return
+     */
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR)
+            return tree[treeIndex];
+
+        int mid = l + (r - l) / 2;
+        int leftChild = leftChild(treeIndex);
+        int rightChild = rightChild(treeIndex);
+        if (queryR <= mid)//左子树查找
+            return query(leftChild, l, mid, queryL, queryR);
+        else if (queryL >= mid + 1)//又子树查找
+            return query(rightChild, mid + 1, r, queryL, queryR);
+        else {//分段查找
+            E leftResult = query(leftChild, l, mid, queryL, mid);
+            E rightResult = query(rightChild, mid + 1, r, mid + 1, queryR);
+            return merger.merger(leftResult, rightResult);
+        }
     }
 
     public int size() {
@@ -67,4 +110,16 @@ public class SegmentTree<E> {
         return 2 * index + 2;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("[");
+        for (int i = 0; i < tree.length; i++) {
+            if (tree[i] != null)
+                res.append(tree[i] + ",");
+            else
+                res.append("null,");
+        }
+        return res.toString();
+    }
 }
